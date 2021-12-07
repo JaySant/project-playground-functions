fe625fad98bc6c1c6a24e8e02c1ea650966285bb
Jay Santos <jaysant15@gmail.com>
2021-12-07 01:39:44 -0300

    Desafio 2 - Oficial

--- src/challenges.js
+++ src/challenges.js
@@ -1,4 +1,4 @@
-// Desafio 1
+//* Desafio 1
 function compareTrue(valor1, valor2){
  if (valor1 === true && valor2 === true){ 
     return true; 
@@ -6,11 +6,12 @@ function compareTrue(valor1, valor2){
     return false;
       }
       
-}
+} 
+
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return base * height / 2;
 }
 
 // Desafio 3
