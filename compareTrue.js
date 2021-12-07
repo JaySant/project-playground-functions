26bf09da0f6ce44fc973693770053e2016dfb5eb
Jay Santos <jaysant15@gmail.com>
2021-12-06 23:53:59 -0300

    revisao desafio 1

---
+++ compareTrue.js
@@ -0,0 +1,13 @@
+function compareTrue(valor1, valor2){
+    
+  if (valor1 === true && valor2 === true){ 
+  return true; 
+} else{
+  return false;
+    }
+    
+}
+
+
+
+
--- playground-functions.js
+++
@@ -1,7 +0,0 @@
-function compareTrue(num1, num2){
-    if( num1 === true && num2 === true){
-        return true;
-    }else{
-        return false;
-    }
-};
